98cfb017d7468ff26abe49400841b32f3957e6b4
Jenny Lawrence <jlawrence010@gmail.com>
2018-04-17 16:34:09 -0700

    images change with button press

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var product1Button = document.getElementById('product-1-button');
 var product2Button = document.getElementById('product-2-button');
 var product3Button = document.getElementById('product-3-button');
 
+var product1Image = document.getElementById('product-1-image');
+var product2Image = document.getElementById('product-2-image');
+var product3Image = document.getElementById('product-3-image');
 
 function Product(url) {
   this.url = url;
@@ -14,9 +17,26 @@ function Product(url) {
 
 
 var allProducts = [
-  new Product('img/dog-duck.jpg'),
-  new Product('img/breakfast.jpg'),
-  new Product('img/sweep.jpg'),
+  new Product('images/dog-duck.jpg'),
+  new Product('images/breakfast.jpg'),
+  new Product('images/sweep.jpg'),
+  new Product('images/bag.jpg'),
+  new Product('images/banana.jpg'),
+  new Product('images/bathroom.jpg'),
+  new Product('images/boots.jpg'),
+  new Product('images/bubblegum.jpg'),
+  new Product('images/chair.jpg'),
+  new Product('images/cthulhu.jpg'),
+  new Product('images/dragon.jpg'),
+  new Product('images/pen.jpg'),
+  new Product('images/pet-sweep.jpg'),
+  new Product('images/scissors.jpg'),
+  new Product('images/shark.jpg'),
+  new Product('images/tauntaun.jpg'),
+  new Product('images/unicorn.jpg'),
+  new Product('images/usb.gif'),
+  new Product('images/water-can.jpg'),
+  new Product('images/wine-glass.jpg'),
 ];
 
 var product1 = allProducts[0];
@@ -26,10 +46,16 @@ var product3 = allProducts[2];
 
 product1Button.addEventListener('click', function(e) {
   product1.votes++;
+  product1 = allProducts[Math.floor(Math.random() * allProducts.length)];
+  product1Image.src = product1.url;
 });
 product2Button.addEventListener('click', function(e) {
   product2.votes++;
+  product2 = allProducts[Math.floor(Math.random() * allProducts.length)];
+  product2Image.src = product2.url;
 });
 product3Button.addEventListener('click', function(e) {
   product3.votes++;
+  product3 = allProducts[Math.floor(Math.random() * allProducts.length)];
+  product3Image.src = product3.url;
 });
